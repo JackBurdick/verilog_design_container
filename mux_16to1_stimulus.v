ccf16a86873bdcb81f0349e0215044f8a7e44e7f
Jack Burdick <jackbburdick@gmail.com>
2017-06-12 08:20:32 -0500

    update stimulus code

--- mux_16to1_stimulus.v
+++ mux_16to1_stimulus.v
@@ -1,6 +1,6 @@
 module stimulus_mux_16to1;
-reg In[15:0];
-reg Sel[3:0];
+reg [15:0]In;
+reg [3:0]Sel;
 wire Out;
 
 mux_16to1 C1(Out, In, Sel);
@@ -8,30 +8,15 @@ mux_16to1 C1(Out, In, Sel);
 // inital block
 initial
 begin
-	Sel[3:0] = 4'b0000;
-	In[0] = 1'h0;
-	In[0] = 1'h1;
-	In[0] = 1'h2;
-	In[0] = 1'h3;
-	In[0] = 1'h4;
-	In[0] = 1'h5;
-	In[0] = 1'h6;
-	In[0] = 1'h7;
-	In[0] = 1'h8;
-	In[0] = 1'h9;
-	In[0] = 1'h10;
-	In[0] = 1'h11;
-	In[0] = 1'h12;
-	In[0] = 1'h13;
-	In[0] = 1'h14;
-	In[0] = 1'h15;
+	Sel = 4'b0000;
+	In = 16'b1111000011110000;
 end
 
 // assignment
 initial
 begin
-	#5 Sel[3:0] = 4'b0001;
-	#5 Sel[3:0] = 4'b0010;
+	#5 Sel = 4'b0001;
+	#5 Sel = 4'b0010;
 	#5 Sel = 4'b0011;
 	#5 Sel = 4'b0100;
 	#5 Sel = 4'b0101;
@@ -51,7 +36,7 @@ end
 // monitor
 initial
 begin
-	$monitor($time, "Out = %h, Sel = %b", Out, Sel);
+	$monitor($time, " Out = %b, Sel = %d", Out, Sel);
 end
 
 
