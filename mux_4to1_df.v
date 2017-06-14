f2d98c84a27517aff2e7c555cac8c94be7c2a6f4
Jack Burdick <jackbburdick@gmail.com>
2017-06-13 22:27:01 -0400

    create mux 4 to 1 using df

--- mux_4to1_df.v
+++ mux_4to1_df.v
@@ -1,4 +1,15 @@
-module mux_4to1_df(Out, In, Sel)
+module mux_4to1_df(Out, In, Sel);
 output Out;
 input [3:0]In;
 input [1:0]Sel;
+
+/*
+ 00-In[0]
+ 01-In[1]
+ 10-In[2]
+ 11-In[3]
+*/
+
+assign Out = Sel[1]&(~Sel[0]&In[2])|(Sel[0]&In[3])|~Sel[1]&(~Sel[0]&In[0])|(Sel[0]&In[1]);
+
+endmodule
